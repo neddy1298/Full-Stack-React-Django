4ba4431835de07cb36e9620da0168c0b5805a3a2
Neddy <neddy1298@gmail.com>
2020-06-12 21:23:04 +0700

    Redux & HTTP

--- leadmanager/frontend/src/components/layout/Header.js
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -3,23 +3,45 @@ import React, { Component } from "react";
 export class Header extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+      <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+        <a className="navbar-brand" href="#">
+          Lead Manager
+        </a>
         <button
           className="navbar-toggler"
           type="button"
           data-toggle="collapse"
-          data-target="#navbarTogglerDemo01"
-          aria-controls="navbarTogglerDemo01"
+          data-target="#navbarColor01"
+          aria-controls="navbarColor01"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a className="navbar-brand" href="#">
-            Lead Managers
-          </a>
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
+
+        <div className="collapse navbar-collapse" id="navbarColor01">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     );
--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,13 +1,71 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addLead } from "../../actions/leads";
 
 export class Form extends Component {
+  state = {
+    name: "",
+    email: "",
+    message: "",
+  };
+
+  static propTypes = {
+    addLead: PropTypes.func.isRequired,
+  };
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, message } = this.state;
+    const lead = { name, email, message };
+    this.props.addLead(lead);
+  };
+
   render() {
+    const { name, email, message } = this.state;
     return (
-      <div>
-        <h1>Add Lead Form</h1>
-      </div>
+      <form onSubmit={this.onSubmit}>
+        <legend>Add Lead</legend>
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            className="form-control"
+            type="text"
+            name="name"
+            onChange={this.onChange}
+            value={name}
+          />
+        </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            className="form-control"
+            type="email"
+            name="email"
+            onChange={this.onChange}
+            value={email}
+          />
+        </div>
+        <div className="form-group">
+          <label>Message</label>
+          <textarea
+            rows="3"
+            className="form-control"
+            type="text"
+            name="message"
+            onChange={this.onChange}
+            value={message}
+          ></textarea>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     );
   }
 }
 
-export default Form;
+export default connect(null, { addLead })(Form);
